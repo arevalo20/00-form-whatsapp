0d16aa0143f0fa47bf9667506e6f1dd8db6de734
Hiram <hiram@upscalemedia.mx>
2020-03-24 15:25:44 -0600

    formulario

--- js/form.js
+++ js/form.js
@@ -1,12 +1,13 @@
-document.querySelector('#submit').addEventListener('click',function(){
-    
+document.querySelector('#submit').addEventListener('click', function () {
+
     let cliente = document.querySelector('#cliente').value;
     let fecha = document.querySelector('#fecha').value;
     let hora = document.querySelector('#hora').value;
     let barbero = document.querySelector('#barbero').value;
     let servicio = document.querySelector('#servicio').value;
+    let imgwhatsapp = document.querySelector('#imgwhatsapp').value;
 
-    let url = "https://api.whatsapp.com/send?phone=522224738550&text=*_La Neta Noticias_*%0A*Reservas*%0A%0A*¿Cual es tu nombre?*%0A" + cliente + "%0A*Indica la fecha de tu reserva*%0A" + fecha + "%0A*Indica la hora de tu reserva*%0A" + hora + "%0A*Barbero de preferencia*%0A" + barbero + "%0A*¿Cual es el servicio que se desea realizar?*%0A" + servicio;
+    let url = "https://api.whatsapp.com/send?phone=522224738550&text=*_La Neta Noticias_*%0A*Reservas*%0A%0A*¿Cual es tu nombre?*%0A" + cliente + "%0A*Indica la fecha de tu reserva*%0A" + fecha + "%0A*Indica la hora de tu reserva*%0A" + hora + "%0A*Barbero de preferencia*%0A" + barbero + "%0A*¿Cual es el servicio que se desea realizar?*%0A" + servicio + imgwhatsapp;
     window.open(url);
 
 });
